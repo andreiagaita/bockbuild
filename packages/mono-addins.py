ee8ebd2e56c1a08d4e4c08a1bad45caea626dabb
David Nielsen <gnomeuser@gmail.com>
2012-04-19 14:19:39 -0300

    Use mono-addins 0.6.2 rather than git snapshot

--- packages/mono-addins.py
+++ packages/mono-addins.py
@@ -1,4 +1,5 @@
-GitHubTarballPackage ('mono', 'mono-addins', '0.6.2+3177b', '3177b22e958c82bf2c5655ac3d4ef7ad25cc2588',
-	configure = './autogen.sh --prefix=%{prefix}',
-	override_properties = { 'make': 'make' }
+Package ('mono-addins', '0.6.2',
+	sources = [
+		'http://download.mono-project.com/sources/mono-addins/%{name}-%{version}.tar.bz2'
+	]
 )
