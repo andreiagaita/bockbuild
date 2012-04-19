956428942e0e26c670f0c6be126c46d50ab5a03f
David Nielsen <gnomeuser@gmail.com>
2012-04-19 14:20:59 -0300

    Add gtk-mac-integration

---
+++ packages/gtk-mac-integration.py
@@ -0,0 +1,5 @@
+GnomeXzPackage ('gtk-mac-integration', '1.0', '1',
+        sources = [
+                'ftp://ftp.gnome.org/pub/gnome/sources/gtk-mac-integration/1.0/%{name}-%{version}.tar.xz'
+        ]
+)
