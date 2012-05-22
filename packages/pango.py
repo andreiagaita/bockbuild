f0e4e35e9c7e0c3ff2edeb9b0bc6fbc07e3b7097
Timo Dörr <timo@latecrew.de>
2012-05-22 16:19:33 +0200

    [pango] revert back to 1.29.5 due to bug 647969
    Pango 1.30.0 crashes banshee on mac, even the patch:
    http://git.gnome.org/browse/pango/patch/?id=216d03ba5023b247515e2adf5df658c0e4e90b3d
    makes no difference
    
    See gnome bugzilla: https://bugzilla.gnome.org/show_bug.cgi?id=647969

--- packages/pango.py
+++ packages/pango.py
@@ -1,9 +1,9 @@
-class PangoPackage (GnomeXzPackage):
+class PangoPackage (GnomePackage):
 	def __init__ (self):
 		GnomePackage.__init__ (self,
 			'pango',
-			version_major = '1.30',
-			version_minor = '0',
+			version_major = '1.29',
+			version_minor = '5',
 			configure_flags = [
 				'--without-x'
 			]
@@ -19,17 +19,27 @@ class PangoPackage (GnomeXzPackage):
 			# revert git 0e091322, it requires unstable glib simply for some deprecation marker macros
 			'patches/pango-stable-glib.patch',
 
-			# Post-1.30.0 commits from git
+			# 3 
+			# [Bug 664125] - Zero-width spaces cause missing characters
+			'http://git.gnome.org/browse/pango/patch/?id=dbf40154eb5804f4e8c582f12b30b8291c9c3532',
+	
+			# [Bug 647969] CoreText patches disabled due to blocker/crasher: 
+			# https://bugzilla.gnome.org/show_bug.cgi?id=647969
 
-			# Use same font family name fallback in pango_core_text_font_map_init
-			'http://git.gnome.org/browse/pango/patch/?id=216d03ba5023b247515e2adf5df658c0e4e90b3d',
+			# CoreText backend: implement font fallbacks
+			#'http://git.gnome.org/browse/pango/patch/?id=37e74619215ede8a4fa7f5edabab14b517e673b2',
+
+			# Make CoreText backend more robust against broken fonts
+			#'http://git.gnome.org/browse/pango/patch/?id=38ada127bfb53911ecd64ced26fd23ec67138b43',
 		])
 
 	def prep (self):
 		GnomePackage.prep (self)
 		self.sh ('patch -p0 < "%{sources[1]}"')
-		if Package.profile.name == 'darwin':
-			for p in range (2, len (self.sources)):
-				self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
+		self.sh ('patch -p1 < "%{sources[2]}"')
+		self.sh ('patch -p1 < "%{sources[3]}"')
+		#if Package.profile.name == 'darwin':
+		#	for p in range (4, len (self.sources)):
+		#		self.sh ('patch -p1 < "%{sources[' + str (p) + ']}"')
 
 PangoPackage ()
